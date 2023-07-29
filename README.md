c323b22cb0cddd17235ca7c59639d7057664f1f1
serinochloe <serinochloe@gmail.com>
2023-07-29 14:48:38 -0600

    updated colors, built basic html out for 9-5, added event listener for save button

--- README.md
+++ README.md
@@ -2,14 +2,13 @@
 
 ## About 
 
-Test your coding prowess with this multiple choice quiz. The quiz demonstrates Javascript functions and my ability to work within the browser-provided API. 
+
 
 ## Link
-<https://serinochloe.github.io/chloes-coding-quiz/>
+
 
 ## Photo 
-<img width="301" alt="Screenshot 2023-07-27 at 10 48 58 PM" src="https://github.com/serinochloe/chloes-coding-quiz/assets/130313684/0b119f01-6bf5-4cde-9a8a-34c79313e46a">
+
 
 ## Sources 
 
-Questions take from codeconquest's practice quizzes: <https://www.codeconquest.com/coding-quizzes>
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,14 @@ $(function () {
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
+
+    // save function 
+    
+    // save listener 
+$(".saveBtn").click(function(event){
+        event.preventDefault();
+        console.log(this);// action goes here!!
+      });
     //
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
