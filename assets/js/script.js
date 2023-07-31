8000c3b33b46fecba7c6991d48246754138cd72e
serinochloe <serinochloe@gmail.com>
2023-07-31 11:25:14 -0600

    Set daysjs to get the current hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,6 +38,8 @@ $(".saveBtn").click(function(event) {
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
+var currentHour = dayjs().hour()
+console.log(currentHour);
     //
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
